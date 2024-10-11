26a73ea202f918b7f22333a1ecada6bdcf6e78e2
BernardoLander <bernardoh.landerl@gmail.com>
2024-10-11 03:57:22 -0400

    1st Commit :rocket:

---
+++ Proyecto1SO_Feliciani_Lander_Salima/src/main/java/Proyecto1SO_Feliciani_Lander_Salima.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author berna
+ */
+public class Proyecto1SO_Feliciani_Lander_Salima {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
